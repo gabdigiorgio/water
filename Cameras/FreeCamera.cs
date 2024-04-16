eff32c167860d08deb8ca6b86b4e5cb4ecd27bf7
gabdigiorgio <gdigiorgio@frba.utn.edu.ar>
2024-04-16 13:44:17 -0300

    First commit.

---
+++ Cameras/FreeCamera.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+
+namespace Water.Cameras
+{
+    internal class FreeCamera : Camera
+    {
+        private readonly bool _lockMouse;
+        private readonly Point _screenCenter;
+        private bool _changed;
+        private Vector2 _pastMousePosition;
+        private float _pitch;
+        private float _yaw = -90f;
+
+        public FreeCamera(float aspectRatio, Vector3 position, Point screenCenter) : this(aspectRatio, position)
+        {
+            _lockMouse = true;
+            this._screenCenter = screenCenter;
+        }
+        
+        public FreeCamera(float aspectRatio, Vector3 position) : base(aspectRatio)
+        {
+            Position = position;
+            _pastMousePosition = Mouse.GetState().Position.ToVector2();
+            UpdateCameraVectors();
+            CalculateView();
+        }
+
+        private float MovementSpeed { get; set; } = 100f;
+        private float MouseSensitivity { get; set; } = 5f;
+
+        private void CalculateView()
+        {
+            View = Matrix.CreateLookAt(Position, Position + FrontDirection, UpDirection);
+        }
+
+        public override void Update(GameTime gameTime)
+        {
+            var elapsedTime = (float) gameTime.ElapsedGameTime.TotalSeconds;
+            _changed = false;
+            ProcessKeyboard(elapsedTime);
+            ProcessMouseMovement(elapsedTime);
+
+            if (_changed)
+                CalculateView();
+        }
+
+        private void ProcessKeyboard(float elapsedTime)
+        {
+            var keyboardState = Keyboard.GetState();
+
+            var currentMovementSpeed = MovementSpeed;
+            if (keyboardState.IsKeyDown(Keys.LeftShift))
+                currentMovementSpeed *= 5f;
+
+            if (keyboardState.IsKeyDown(Keys.A) || keyboardState.IsKeyDown(Keys.Left))
+            {
+                Position += -RightDirection * currentMovementSpeed * elapsedTime;
+                _changed = true;
+            }
+
+            if (keyboardState.IsKeyDown(Keys.D) || keyboardState.IsKeyDown(Keys.Right))
+            {
+                Position += RightDirection * currentMovementSpeed * elapsedTime;
+                _changed = true;
+            }
+
+            if (keyboardState.IsKeyDown(Keys.W) || keyboardState.IsKeyDown(Keys.Up))
+            {
+                Position += FrontDirection * currentMovementSpeed * elapsedTime;
+                _changed = true;
+            }
+
+            if (keyboardState.IsKeyDown(Keys.S) || keyboardState.IsKeyDown(Keys.Down))
+            {
+                Position += -FrontDirection * currentMovementSpeed * elapsedTime;
+                _changed = true;
+            }
+        }
+
+        private void ProcessMouseMovement(float elapsedTime)
+        {
+            var mouseState = Mouse.GetState();
+
+            if (mouseState.RightButton.Equals(ButtonState.Pressed))
+            {
+                var mouseDelta = mouseState.Position.ToVector2() - _pastMousePosition;
+                mouseDelta *= MouseSensitivity * elapsedTime;
+
+                _yaw -= mouseDelta.X;
+                _pitch += mouseDelta.Y;
+
+                if (_pitch > 89.0f)
+                    _pitch = 89.0f;
+                if (_pitch < -89.0f)
+                    _pitch = -89.0f;
+
+                _changed = true;
+                UpdateCameraVectors();
+
+                if (_lockMouse)
+                {
+                    Mouse.SetPosition(_screenCenter.X, _screenCenter.Y);
+                    Mouse.SetCursor(MouseCursor.Crosshair);
+                }
+                else
+                {
+                    Mouse.SetCursor(MouseCursor.Arrow);
+                }
+            }
+
+            _pastMousePosition = Mouse.GetState().Position.ToVector2();
+        }
+
+        private void UpdateCameraVectors()
+        {
+            Vector3 tempFront;
+            
+            tempFront.X = MathF.Cos(MathHelper.ToRadians(_yaw)) * MathF.Cos(MathHelper.ToRadians(_pitch));
+            tempFront.Y = MathF.Sin(MathHelper.ToRadians(_pitch));
+            tempFront.Z = MathF.Sin(MathHelper.ToRadians(_yaw)) * MathF.Cos(MathHelper.ToRadians(_pitch));
+            
+            FrontDirection = Vector3.Normalize(tempFront);
+            
+            RightDirection = Vector3.Normalize(Vector3.Cross(FrontDirection, Vector3.Up));
+            UpDirection = Vector3.Normalize(Vector3.Cross(RightDirection, FrontDirection));
+        }
+    }
+}
---
+++ Game1.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Water.Cameras;
+
+namespace Water
+{
+    public class Game1 : Game
+    {
+        public const string ContentFolder3D = "Models/";
+        public const string ContentFolderEffects = "Effects/";
+        public const string ContentFolderMusic = "Music/";
+        public const string ContentFolderSounds = "Sounds/";
+        public const string ContentFolderSpriteFonts = "SpriteFonts/";
+        public const string ContentFolderTextures = "Textures/";
+        
+        private readonly GraphicsDeviceManager _graphicsDeviceManager;
+        
+        private FreeCamera _freeCamera;
+        private readonly Vector3 _cameraInitialPosition = new(0f, 50f, 300f);
+
+        public Game1()
+        {
+            _graphicsDeviceManager = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+            Window.AllowUserResizing = true;
+            Window.Title = "Water";
+        }
+
+        protected override void Initialize()
+        {
+            _graphicsDeviceManager.PreferredBackBufferWidth = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Width - 100;
+            _graphicsDeviceManager.PreferredBackBufferHeight = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Height - 100;
+            _graphicsDeviceManager.ApplyChanges();
+            
+            // Camera
+            var screenSize = new Point(GraphicsDevice.Viewport.Width / 2, GraphicsDevice.Viewport.Height / 2);
+            _freeCamera = new FreeCamera(GraphicsDevice.Viewport.AspectRatio, _cameraInitialPosition, screenSize);
+            
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            var keyboardState = Keyboard.GetState();
+
+            if (keyboardState.IsKeyDown(Keys.Escape))
+            {
+                Exit();
+            }
+            
+            _freeCamera.Update(gameTime);
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            base.Draw(gameTime);
+        }
+    }
+}
