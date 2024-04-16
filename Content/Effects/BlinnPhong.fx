3a5efac5ab001ea2afced0f622e20a23dd222032
gabdigiorgio <gdigiorgio@frba.utn.edu.ar>
2024-04-16 14:24:58 -0300

    Teapot draw.

---
+++ Content/Effects/BlinnPhong.fx
@@ -0,0 +1,85 @@
+﻿#if OPENGL
+#define SV_POSITION POSITION
+#define VS_SHADERMODEL vs_3_0
+#define PS_SHADERMODEL ps_3_0
+#else
+#define VS_SHADERMODEL vs_4_0_level_9_1
+#define PS_SHADERMODEL ps_4_0_level_9_3
+#endif
+
+// Matrices
+float4x4 World;
+float4x4 View;
+float4x4 Projection;
+float4x4 InverseTransposeWorld;
+ 
+// Light related
+float3 Color;
+
+float3 LightPosition;
+float3 EyePosition;
+
+float3 AmbientColor;
+float KAmbient;
+ 
+float3 DiffuseColor;
+float KDiffuse;
+
+float3 SpecularColor;
+float KSpecular;
+float Shininess;
+ 
+struct VertexShaderInput
+{
+    float4 Position : POSITION0;
+    float4 Normal : NORMAL;
+};
+ 
+struct VertexShaderOutput
+{
+    float4 Position : POSITION0;
+    float3 WorldPosition : TEXCOORD0;
+    float3 Normal : TEXCOORD1;
+};
+ 
+VertexShaderOutput MainVS(VertexShaderInput input)
+{
+    VertexShaderOutput output = (VertexShaderOutput) 0;
+ 
+    float4 worldPosition = mul(input.Position, World);
+    float4 viewPosition = mul(worldPosition, View);
+    output.Position = mul(viewPosition, Projection);
+    output.WorldPosition = worldPosition;
+    output.Normal = mul(input.Normal, InverseTransposeWorld);
+ 
+    return output;
+}
+ 
+float4 MainPS(VertexShaderOutput input) : COLOR0
+{
+    float3 lightDirection = normalize(LightPosition - input.WorldPosition);
+    float3 viewDirection = normalize(EyePosition - input.WorldPosition);
+    float3 halfVector = normalize(lightDirection + viewDirection); 
+    
+    float3 ambientLight = KAmbient * AmbientColor;
+    
+    float NdotL = saturate(dot(input.Normal, lightDirection));
+    float3 diffuseLight = KDiffuse * DiffuseColor * NdotL;
+    
+    float NdotH = saturate(dot(input.Normal, halfVector));
+    float3 specularLight = sign(NdotL) * KSpecular * SpecularColor * pow(NdotH, Shininess);
+
+    float4 finalColor = float4((ambientLight + diffuseLight) * Color + specularLight, 1.0);
+    
+    return finalColor;
+}
+
+ 
+technique BasicColorDrawing
+{
+    pass Pass0
+    {
+        VertexShader = compile VS_SHADERMODEL MainVS();
+        PixelShader = compile PS_SHADERMODEL MainPS();
+    }
+}
--- Game1.cs
+++ Game1.cs
@@ -1,7 +1,9 @@
-﻿using Microsoft.Xna.Framework;
+﻿using System;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using Water.Cameras;
+using Water.Geometries;
 
 namespace Water
 {
@@ -16,11 +18,20 @@ namespace Water
         
         private readonly GraphicsDeviceManager _graphicsDeviceManager;
 
+        // Camera
         private FreeCamera _freeCamera;
         private readonly Vector3 _cameraInitialPosition = new(0f, 50f, 300f);
         
+        private readonly Vector3 _lightPosition = new(500f, 50f, 300f);
+        
+        // Skybox
         private SkyBox _skyBox;
         private const int SkyBoxSize = 2000;
+        
+        // Teapot
+        private TeapotPrimitive _teapot;
+        private Matrix _teapotWorld;
+        private Effect _blinnPhong;
 
         public Game1()
         {
@@ -41,6 +52,11 @@ namespace Water
             var screenSize = new Point(GraphicsDevice.Viewport.Width / 2, GraphicsDevice.Viewport.Height / 2);
             _freeCamera = new FreeCamera(GraphicsDevice.Viewport.AspectRatio, _cameraInitialPosition, screenSize);
             
+            // Teapot
+            _teapot = new TeapotPrimitive(GraphicsDevice, 100f, 16);
+            var teapotPosition = new Vector3(0f, 50f, 0f);
+            _teapotWorld = Matrix.CreateRotationY(MathF.PI/2) * Matrix.CreateTranslation(teapotPosition);
+            
             base.Initialize();
         }
 
@@ -50,6 +66,8 @@ namespace Water
             var skyBoxTexture = Content.Load<TextureCube>(ContentFolderTextures + "/skyboxes/mountain_skybox");
             var skyBoxEffect = Content.Load<Effect>(ContentFolderEffects + "SkyBox");
             _skyBox = new SkyBox(skyBox, skyBoxTexture, skyBoxEffect, SkyBoxSize);
+            
+            _blinnPhong = Content.Load<Effect>(ContentFolderEffects + "BlinnPhong");
         }
 
         protected override void Update(GameTime gameTime)
@@ -71,6 +89,8 @@ namespace Water
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             DrawSkyBox(_freeCamera.View, _freeCamera.Projection, _freeCamera.Position);
+            
+            DrawTeapot(_teapotWorld, _freeCamera.View, _freeCamera.Projection, _freeCamera.Position);
 
             base.Draw(gameTime);
         }
@@ -83,6 +103,39 @@ namespace Water
             GraphicsDevice.RasterizerState = rasterizerState;
             _skyBox.Draw(view, projection, position);
             GraphicsDevice.RasterizerState = originalRasterizerState;
+        }
+        
+        private void DrawTeapot(Matrix world, Matrix view, Matrix projection, Vector3 position)
+        {
+            var previousRasterizerState = GraphicsDevice.RasterizerState;
+            GraphicsDevice.RasterizerState = RasterizerState.CullNone;
+            
+            _blinnPhong.CurrentTechnique = _blinnPhong.Techniques["BasicColorDrawing"];
+            
+            _blinnPhong.Parameters["Color"].SetValue(Color.Green.ToVector3());
+            
+            _blinnPhong.Parameters["World"].SetValue(world);
+            _blinnPhong.Parameters["View"].SetValue(view);
+            _blinnPhong.Parameters["Projection"].SetValue(projection);
+            _blinnPhong.Parameters["InverseTransposeWorld"].SetValue(Matrix.Invert(Matrix.Transpose(world)));
+            
+            _blinnPhong.Parameters["AmbientColor"].SetValue(Color.White.ToVector3());
+            _blinnPhong.Parameters["KAmbient"].SetValue(0.3f);
+            
+            _blinnPhong.Parameters["LightPosition"].SetValue(_lightPosition);
+            
+            _blinnPhong.Parameters["DiffuseColor"].SetValue(Color.White.ToVector3());
+            _blinnPhong.Parameters["KDiffuse"].SetValue(0.7f);
+            
+            _blinnPhong.Parameters["EyePosition"].SetValue(position);
+            
+            _blinnPhong.Parameters["SpecularColor"].SetValue(Color.White.ToVector3());
+            _blinnPhong.Parameters["KSpecular"].SetValue(1f);
+            _blinnPhong.Parameters["Shininess"].SetValue(32f);
+            
+            _teapot.Draw(_blinnPhong);
+
+            GraphicsDevice.RasterizerState = previousRasterizerState;
         }
     }
 }
